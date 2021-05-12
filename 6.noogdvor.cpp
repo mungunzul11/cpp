9c21e62144bb3be88ec7854e536e8ff6b0e90242
mungunzul11 <82897778+mungunzul11@users.noreply.github.com>
2021-05-13 01:33:45 +0800

    Add files via upload

---
+++ 6.noogdvor.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+	    int num,num1,noogdvor;
+	    cout<<"too oruul: ";
+	    cin>>num;
+	    cout<<"oor too oruul: ";
+	    cin>>num1;
+	    noogdvor=num/num1;
+	    cout<<"tanii oruulsan toonuudiin noogdvor: "<<noogdvor;
+	    return 0;
+	    
+}
